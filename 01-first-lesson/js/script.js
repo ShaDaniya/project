e8c5b802db55498ad54c434fd40c7bdd0f941a8c
daniya <example@gmail.com>
2023-02-15 20:46:30 +0300

    first commit

---
+++ 01-first-lesson/js/script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18", "18");
+// console.log(answer + 5);
+
+//const answers = [];
+
+// answers[0] = prompt("Как ваше имя", "");
+// answers[1] = prompt("Как ваша фамилия", "");
+// answers[2] = prompt("Сколько вам лет", "");
+
+//document.write(answers);
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+
+//Интерполяция
+
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Daniya';
+
+// alert(`Privet, ${user}`);
+
+
+//ОПЕРАТОРЫ JS
+
+
+console.log('arr' + " - object");
+console.log(4 + " - object");
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(incr++);
+console.log(decr--);
