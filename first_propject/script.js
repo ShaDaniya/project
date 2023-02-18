cd58f083bb7c654b62df2e16ebffa83e40900db9
daniya <example@gmail.com>
2023-02-18 18:03:53 +0300

    added conditional

--- first_propject/script.js
+++ first_propject/script.js
@@ -10,16 +10,35 @@ const personalMovieDB = {
     privat: false
 };
 
-const lastMovie = prompt("Один из последних просмотренных фильмов?", "");
-const movieRaitng = prompt("На сколько оцените его?", "");
-const lastMoviwTwo= prompt("Один из последних просмотренных фильмов?", "");
-const movieRaitngTwo = prompt("На сколько оцените его?", "");
+// const lastMovie = prompt("Один из последних просмотренных фильмов?", "");
+// const movieRaitng = prompt("На сколько оцените его?", "");
+// const lastMoviwTwo= prompt("Один из последних просмотренных фильмов?", "");
+// const movieRaitngTwo = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[lastMovie] = movieRaitng;
-personalMovieDB.movies[lastMoviwTwo] = movieRaitngTwo
+// personalMovieDB.movies[lastMovie] = movieRaitng;
+// personalMovieDB.movies[lastMoviwTwo] = movieRaitngTwo;
 
-console.log(personalMovieDB.count);
-console.log(personalMovieDB.movies);
+for (let i = 0; i < 2; i++) {
+    const lastMovie = prompt("Один из последних просмотренных фильмов?", "");
+    const movieRaitng = prompt("На сколько оцените его?", "");
+
+    if (lastMovie != '' && movieRaitng != '' && lastMovie != null && movieRaitng != null && lastMovie.length < 50) {
+        personalMovieDB.movies[lastMovie] = movieRaitng;
+    } else {
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
 console.log(personalMovieDB);
 
 
